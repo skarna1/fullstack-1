b929564809609cfbf04c4316d6edd25bbef1c894
Sami K <sami.karna@gmail.com>
2018-07-08 19:37:08 +0300

    Add solution to task 1.3

--- tehtava1/src/index.js
+++ tehtava1/src/index.js
@@ -10,16 +10,16 @@ const Otsikko = (props) => {
 
 const Osa = (props) => {
     return (
-        <p>{props.sisalto} {props.tehtavia}</p>
+        <p>{props.osa.nimi} {props.osa.tehtavia}</p>
     )
 }
 
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa sisalto={props.osa1} tehtavia={props.tehtavia1} />
-            <Osa sisalto={props.osa2} tehtavia={props.tehtavia2} />
-            <Osa sisalto={props.osa3} tehtavia={props.tehtavia3} />
+            <Osa osa={props.osa1}/>
+            <Osa osa={props.osa2} />
+            <Osa osa={props.osa3} />
         </div>
     )
 }
@@ -32,20 +32,26 @@ const Yhteensa = (props) => {
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
 
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} tehtavia1={tehtavia1}
-                osa2={osa2} tehtavia2={tehtavia2}
-                osa3={osa3} tehtavia3={tehtavia3} />
-            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3} />
+            <Sisalto osa1={osa1}
+                osa2={osa2}
+                osa3={osa3} />
+            <Yhteensa yhteensa={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
         </div>
     )
 }
